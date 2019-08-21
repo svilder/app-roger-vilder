9845b7264b9fe3726a81fd6f8f8378376428b195
Sandra Vilder <sandra.vilder@gmail.com>
2019-08-21 17:11:26 +0200

    controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :contact]
 
   def home
+  end
+
+  def contact
+  end
+
+  def update_bio
   end
 end
---
+++ app/controllers/pieces_controller.rb
@@ -0,0 +1,29 @@
+class PiecesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @pieces = Piece.all
+  end
+
+  def show
+  end
+
+  def new
+    @piece = Piece.new
+  end
+
+  def create
+    @piece = Piece.new(piece_params)
+    if @piece.save
+      redirect_to pieces_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def piece_params
+    params.require(:piece).permit(:name, :description, :dimensions, :year, :category, :collection, :image)
+  end
+end

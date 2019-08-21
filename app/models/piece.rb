a20a246d15c7397e0c9bfc5429139b9f152937d3
Sandra Vilder <sandra.vilder@gmail.com>
2019-08-21 17:08:33 +0200

    Models, routes

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,2 +1,10 @@
 class Piece < ApplicationRecord
+  CATEGORIES = ["Kinetics", "Statics"]
+  COLLECTIONS = ["Kinetic", "Sculptur", "Drawing", "Photo", "Animation", "Video"]
+
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
+  validates :collection, presence: true, inclusion: { in: COLLECTIONS }
+  validates :name, presence: true
+  validates :year, presence: true
+  validates :dimensions, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :pieces, only: [:new, :create]
+
+  scope '(:locale)', locale: /fr|en/ do
+    root to: 'pages#home'
+    get 'contact', to: 'pages#contact', as: :contact
+
+    resources :pieces, only: [:index, :show]
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end

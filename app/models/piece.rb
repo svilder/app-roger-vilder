a072f9e321a139622e82fa99a599ac16fa43e704
Sandra Vilder <sandra.vilder@gmail.com>
2019-09-26 18:11:08 +0200

    model validations

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,12 +1,12 @@
 class Piece < ApplicationRecord
-  # CATEGORIES = ["Kinetics", "Statics"]
- #  COLLECTIONS = ["Kinetic", "Sculptur", "Drawing", "Photo", "Animation", "Video"]
+   CATEGORIES = ["Kinetics", "Statics"]
+   COLLECTIONS = ["Kinetic", "Sculptur", "Drawing", "Photo", "Animation", "Video"]
 
-  # validates :category, presence: true, inclusion: { in: CATEGORIES }
-  # validates :collection, presence: true, inclusion: { in: COLLECTIONS }
-  # validates :name, presence: true
-  # validates :year, presence: true
-  # validates :dimensions, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
+  validates :collection, presence: true, inclusion: { in: COLLECTIONS }
+  validates :name, presence: true
+  validates :year, presence: true
+  validates :dimensions, presence: true
 
   mount_uploader :photo, PhotoUploader
 end

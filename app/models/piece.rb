f12cd41b3a0bd18a2cc64a355b1c82d44e05ee2f
Sandra Vilder <sandra.vilder@gmail.com>
2019-09-26 18:17:20 +0200

    try without image tag

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,6 +1,6 @@
 class Piece < ApplicationRecord
-   CATEGORIES = ["Kinetics", "Statics"]
-   COLLECTIONS = ["Kinetic", "Sculptur", "Drawing", "Photo", "Animation", "Video"]
+  CATEGORIES = ["Kinetics", "Statics"]
+  COLLECTIONS = ["Kinetic", "Sculptur", "Drawing", "Photo", "Animation", "Video"]
 
   validates :category, presence: true, inclusion: { in: CATEGORIES }
   validates :collection, presence: true, inclusion: { in: COLLECTIONS }

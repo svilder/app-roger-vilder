1b220228dbf4b32556c9203940a8b1017da33126
Sandra Vilder <sandra.vilder@gmail.com>
2019-12-02 19:24:07 +0100

    no locale for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  before_action :set_locale
 
   def default_url_options
     { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
